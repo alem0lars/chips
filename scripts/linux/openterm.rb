677bfb6ee124d4ccd174f5505461157e74547d18
Alessandro Molari <molari.alessandro@gmail.com>
2016-11-13 11:43:51 +0100

    fix openterm script

--- scripts/linux/openterm.rb
+++ scripts/linux/openterm.rb
@@ -1,24 +1,28 @@
 config = "openterm".get_config
 
 options = parse_args do |parser, options|
-  parser.on("-t", "--[no-]tmux", "use tmux") do |kernel|
+  parser.on("-t", "--[no-]tmux", "use tmux") do |tmux|
     options[:tmux] = tmux
   end
-  parser.on("-c", "--cmd COMMAND", "command") do |kernel|
+  parser.on("-c", "--cmd COMMAND", "command") do |cmd|
     options[:cmd] = cmd
   end
-  parser.on("-t", "--title TITLE", "title") do |kernel|
+  parser.on("-t", "--title TITLE", "title") do |title|
     options[:title] = title
   end
 end
 
-tmux  = config[:tmux] unless options.has_key?(:tmux)
 term  = config[:term]
-title = options[:title]
+if options.has_key? :tmux
+  tmux = options[:tmux]
+else
+  tmux = config[:tmux]
+end
+title = options[:title] || ("a".."z").to_a.shuffle[0,8].join
 cmd   = options[:cmd]
 
 args  = []
-args += ["-title", options[:title]] if options[:title]
+args += ["-title", title]
 args += ["-e", "zsh", "-i"]
 args += ["-c", "tmux new-session -s #{title} #{cmd}"] if  cmd &&  tmux
 args += ["-c", "tmux new-session -s #{title}"       ] if !cmd &&  tmux
