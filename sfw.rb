eb6f8f8520e2dbe5496adda84667cee7c2101706
Alessandro Molari <molari.alessandro@gmail.com>
2017-10-12 07:45:12 +0200

    fix download

--- sfw.rb
+++ sfw.rb
@@ -9,6 +9,8 @@ require "ostruct"
 require "pathname"
 require "shellwords"
 require "yaml"
+require "tempfile"
+require "pp"
 
 $exit_code = nil
 
@@ -456,28 +458,30 @@ DOWNLOAD_ERRORS = [
 ]
 
 def download(url, max_size: nil)
-  url = URI.encode(URI.decode(url))
-  url = URI(url)
-  raise Error, "url was invalid" if !url.respond_to?(:open)
+  if $simulate
+    "downloaded url #{url}".pinf
+    ""
+  else
+    url = URI.encode(URI.decode(url))
+    url = URI(url)
+    raise Error, "url was invalid" if !url.respond_to?(:open)
 
-  options = {}
-  options["User-Agent"] = "MyApp/1.2.3"
-  options[:content_length_proc] = ->(size) {
-    if max_size && size && size > max_size
-      raise Error, "file is too big (max is #{max_size})"
+    options = {}
+    options["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
+    options[:content_length_proc] = ->(size) {
+      if max_size && size && size > max_size
+        raise Error, "file is too big (max is #{max_size})"
+      end
+    }
+
+    downloaded_file = url.open(options)
+
+    if downloaded_file.is_a?(StringIO)
+      downloaded_file.read
+    else
+      downloaded_file
     end
-  }
-
-  downloaded_file = url.open(options)
-
-  if downloaded_file.is_a?(StringIO)
-    tempfile = Tempfile.new(basename, binmode: true)
-    IO.copy_stream(downloaded_file, tempfile.path)
-    downloaded_file = tempfile
-    OpenURI::Meta.init downloaded_file, stringio
   end
-
-  downloaded_file
 rescue *DOWNLOAD_ERRORS => error
   raise if error.instance_of?(RuntimeError) && error.message !~ /redirection/
   raise Error, "download failed (#{url}): #{error.message}"
