3ee8e882f5c982db61892cf3baf64cae5e1acacf
Alessandro Molari <molari.alessandro@gmail.com>
2017-11-10 09:02:44 +0100

    add comments

--- scripts/linux/extend-displays.zsh
+++ scripts/linux/extend-displays.zsh
@@ -29,3 +29,14 @@ case "${_pos_of_lowdpi_display}" in
     exit -1
     ;;
 esac
+
+
+# xrandr --output eDP1 --primary --mode 2880x1620 --pos 0x0 --rotate normal \
+# --output DP2-1 --mode 1680x1050 --pos 2880x0 --scale 2x2 --panning 3360x2100 --right-of eDP1 --rotate normal
+#
+# ~
+# xrandr --output DP2-1 --off
+#
+# ~
+# xrandr --output eDP1 --primary --mode 2880x1620 --pos 0x0 --rotate normal \
+# --output DP2-1 --mode 1680x1050 --pos 2880x0 --scale 2x2 --right-of eDP1 --rotate normal
