10785ab165e306d6e4e2d964b26a49dbba67a60a
Alessandro Molari <molari.alessandro@gmail.com>
2017-11-17 14:50:13 +0100

    no message

--- scripts/fzf/gitignore.zsh
+++ scripts/fzf/gitignore.zsh
@@ -5,16 +5,16 @@
 # param is a comma separated list of ignore profiles.
 # If param is ommited choose interactively.
 
-function __gi() {
+function __fzf_gitignore() {
   curl -L -s https://www.gitignore.io/api/"$@"
 }
 
 if  [ "$#" -eq 0 ]; then
   IFS+=","
-  for item in $(__gi list); do
+  for item in $(__fzf_gitignore list); do
     echo $item
   done | fzf --multi --ansi | paste -s -d "," - |
-  { read result && __gi "$result"; }
+  { read result && __fzf_gitignore "$result"; }
 else
-  __gi "$@"
+  __fzf_gitignore "$@"
 fi
