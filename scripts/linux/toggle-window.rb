b33b0d43a6f7ac39dfdd5fb2cab2480f603c2a93
Alessandro Molari <molari.alessandro@gmail.com>
2018-02-08 08:10:37 +0100

    add chip toggle-window :D :D :D

--- scripts/linux/toggle-window.rb
+++ scripts/linux/toggle-window.rb
@@ -2,7 +2,11 @@
   -> () { "xdotool".check_program! },
   -> () { "wmctrl".check_program! },
   -> () {
-    # Parse options.
+    # Init config
+    $config = {}
+  },
+  -> () {
+    # Parse options
     $options = parse_args do |parser, opts|
       parser.on("-c", "--cmd CMD", "Command to be executed") do |cmd|
         opts[:cmd] = cmd
@@ -12,8 +16,32 @@
         opts[:name] = name
       end
     end
+
+    if !$options[:cmd]
+      "Missing command".perr
+    elsif !$options[:name]
+      "Missing name".perr
+    else
+      true
+    end
   },
   -> () {
-    # TODO implement
+    # Normalize config
+    $config[:cmd] = $options[:cmd]
+    $config[:name] = $options[:name]
+
+    true
+  },
+  -> () {
+    window = "xdotool".capture "search", "--name", $config[:name]
+    matches = window.split("\n")
+    if matches.length == 0
+      $config[:cmd].run interactive: true
+    elsif matches.length > 1
+      "Ambiguous name #{name}".perr
+    else
+      match = matches.first
+      "wmctrl".run "-i", "-R", match
+    end
   }
 ].do_all auto_exit_code: true
